a328049bdd79cc99ba131843eed2980a0b6d6d68
Ejiroghene Okpirikhre <alexejirow@gmail.com>
2026-05-22 23:55:12 +0100

    app update

--- much-to-do/Server/MuchToDo/cmd/api/main.go
+++ much-to-do/Server/MuchToDo/cmd/api/main.go
@@ -71,6 +71,8 @@ func main() {
 	}()
 	slog.Info("Successfully connected to MongoDB.")
 
+	fmt.Println("Mongo URI:", cfg.MongoURI)
+
 	// 3. Initialize Services (Cache, Auth)
 	cacheService := cache.NewCacheService(cfg)
 	tokenService := auth.NewTokenService(cfg.JWTSecretKey, cfg.JWTExpirationHours)
